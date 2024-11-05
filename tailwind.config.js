de3582721e471fd51f7e70a0eb0099ad09b04c18
Ahyarnur <ahyarahyar5741@gmail.com>
2024-11-05 10:24:09 +0700

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 // tailwind.config.js
 module.exports = {
+  content: ["./*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        abu: '#eee'
       },
     },
   },
